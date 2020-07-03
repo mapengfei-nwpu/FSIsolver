a59f74ab5072c733d35265599f04a2b4e83e3d9e
Your Name <you@example.com>
2020-07-04 00:56:10 +0800

    finish NSsolver

--- b.py
+++ b.py
@@ -1,6 +1,8 @@
+from fenics import *
+from mshr import *
+import numpy as np
 
-
-def solver(u0, p0, f, bcu, bcp, dt = 0.01, nu = 0.01)
+def NSsolver(u0, p0, f, bcu, bcp, dt = 0.01, nu = 0.01):
 
     # Define function spaces (P2-P1)
     V = u0.function_space()
@@ -55,3 +57,53 @@ def solver(u0, p0, f, bcu, bcp, dt = 0.01, nu = 0.01)
     solve(A3, u1.vector(), b3, "bicgstab", "default")
 
     return u1, p1
+
+def run_solver():
+    "Run solver to compute and post-process solution"
+    T = 5.0
+    num_steps = 5000
+    dt = T / num_steps
+    mu = 0.001
+    # Create mesh
+    channel = Rectangle(Point(0, 0), Point(2.2, 0.41))
+    cylinder = Circle(Point(0.2, 0.2), 0.05)
+    domain = channel - cylinder
+    mesh = generate_mesh(domain, 64)
+
+    # Define function spaces
+    V = VectorFunctionSpace(mesh, 'P', 2)
+    Q = FunctionSpace(mesh, 'P', 1)
+
+    # Define boundaries
+    inflow =    'near(x[0], 0)'
+    outflow =   'near(x[0], 2.2)'
+    walls =     'near(x[1], 0) || near(x[1], 0.41)'
+    cylinder =  'on_boundary && x[0]>0.1 && x[0]<0.3 && x[1]>0.1 && x[1]<0.3'
+
+    # Define inflow profile
+    inflow_profile = ('4.0*1.5*x[1]*(0.41 - x[1]) / pow(0.41, 2)', '0')
+
+    # Define boundary conditions
+    bcu_inflow = DirichletBC(V, Expression(inflow_profile, degree=2), inflow)
+    bcu_walls = DirichletBC(V, Constant((0, 0)), walls)
+    bcu_cylinder = DirichletBC(V, Constant((0, 0)), cylinder)
+    bcp_outflow = DirichletBC(Q, Constant(0), outflow)
+    bcu = [bcu_inflow, bcu_walls, bcu_cylinder]
+    bcp = [bcp_outflow]
+    
+    u0 = Function(V)
+    p0 = Function(Q)
+    f = Constant((0,0))
+
+    # output velocity
+    ufile = File('NSsolver/u.pvd')
+    
+    for n in range(num_steps):
+        u1, p1 = NSsolver(u0, p0, f, bcu, bcp, dt = dt, nu = mu)
+        u0.assign(u1)
+        p0.assign(p1)
+        ufile << u0
+        print("step : ",n)
+
+if __name__ == '__main__':
+    run_solver()
