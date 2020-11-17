e5666b360b9ff6ac0062b411774453de2a887c8b
Your Name <you@example.com>
2020-11-18 03:12:15 +0800

    clean the code again.

--- ParticleInterpolation/ParticleInterPolation_1.cpp
+++ ParticleInterpolation/ParticleInterPolation_1.cpp
@@ -58,31 +58,25 @@ void find_min_max_points(
     max[1] = max_y;
     max[2] = max_z;
 }
+
 void evaluation_at_gauss_points(
     std::shared_ptr<const Function> function,
     std::vector<double> &points_out,
     std::vector<double> &values_out,
     std::vector<double> &weights_out)
 {
-
-
     // Smart shortcut
-    auto order   = quadrature_order;
-    auto mesh    = function->function_space()->mesh();
-    auto dim     = mesh->topology().dim();
+    auto order = quadrature_order;
+    auto mesh = function->function_space()->mesh();
+    auto dim = mesh->topology().dim();
     auto element = function->function_space()->element();
-    auto dofmap  = function->function_space()->dofmap();
+    auto dofmap = function->function_space()->dofmap();
     auto MPI_RANK = MPI::rank(MPI_COMM_WORLD);
     auto MPI_SIZE = MPI::size(MPI_COMM_WORLD);
 
     // Local variables, deleted at the end of the function
-	std::vector<double> coordinates;
+    std::vector<double> coordinates;
     std::vector<double> function_dofs;
-
-    // assert for input valuables
-    assert(values_out.size()==0);
-    assert(points_out.size()==0);    
-    assert(weights_out.size()==0);
 
     // Construct Gauss quadrature rule
     SimplexQuadrature gauss_quadrature(dim, order);
@@ -102,43 +96,46 @@ void evaluation_at_gauss_points(
         // push back gauss points and gauss weights.
         auto quadrature_rule = gauss_quadrature.compute_quadrature_rule(*cell);
 
-        points_out_local.insert(points_out_local.end(),quadrature_rule.first.begin(),quadrature_rule.first.end());
-        weights_out_local.insert(weights_out_local.end(),quadrature_rule.second.begin(),quadrature_rule.second.end());
-        
+        points_out_local.insert(points_out_local.end(), quadrature_rule.first.begin(), quadrature_rule.first.end());
+        weights_out_local.insert(weights_out_local.end(), quadrature_rule.second.begin(), quadrature_rule.second.end());
+
         // push back function dofs on the cell.
         auto dofs = dofmap->cell_dofs(cell->index());
         std::vector<double> cell_function_dofs(dofs.size());
         function->vector()->get_local(cell_function_dofs.data(), dofs.size(), dofs.data());
-        function_dofs_local.insert(function_dofs_local.end(),cell_function_dofs.begin(),cell_function_dofs.end());
+        function_dofs_local.insert(function_dofs_local.end(), cell_function_dofs.begin(), cell_function_dofs.end());
 
         // push back cell coordinates.
         std::vector<double> cell_coordinates;
         cell->get_vertex_coordinates(cell_coordinates);
-        coordinates_local.insert(coordinates_local.end(),cell_coordinates.begin(),cell_coordinates.end());
+        coordinates_local.insert(coordinates_local.end(), cell_coordinates.begin(), cell_coordinates.end());
     }
+
     MPI::gather(MPI_COMM_WORLD, points_out_local, points_out, 0);
     MPI::gather(MPI_COMM_WORLD, weights_out_local, weights_out, 0);
     MPI::gather(MPI_COMM_WORLD, function_dofs_local, function_dofs, 0);
     MPI::gather(MPI_COMM_WORLD, coordinates_local, coordinates, 0);
 
-
-    if (MPI_RANK == 0) { 
+    if (MPI_RANK == 0)
+    {
         size_t value_size = function->value_size();
-        size_t num_cells  = mesh->num_entities_global(dim);
-        size_t num_gauss  = weights_out.size()/num_cells;
-        size_t num_dofs   = function_dofs.size()/num_cells;
-        values_out.resize(points_out.size()/dim*value_size);
+        size_t num_cells = mesh->num_entities_global(dim);
+        size_t num_gauss = weights_out.size() / num_cells;
+        size_t num_dofs = function_dofs.size() / num_cells;
+        values_out.resize(points_out.size() / dim * value_size);
         // this parameter is true if gpu is used.
         PolynomialInterpolation pli(true);
-        pli.evaluate_function(num_cells,num_gauss,value_size,num_dofs,coordinates.data(),
-                            function_dofs.data(),points_out.data(),values_out.data());
-        
-    } else {
+        pli.evaluate_function(num_cells, num_gauss, value_size, num_dofs, coordinates.data(),
+                              function_dofs.data(), points_out.data(), values_out.data());
+    }
+    else
+    {
         points_out.clear();
         values_out.clear();
         weights_out.clear();
     }
 }
+
 void get_gauss_rule(
     bool isSolid,
     std::shared_ptr<const Function> function,
@@ -146,68 +143,55 @@ void get_gauss_rule(
     std::vector<float> &coordinates,
     std::vector<float> &values_weights)
 {
-    assert(coordinates.size()==0);
-    assert(values_weights.size()==0);
-
     auto MPI_SIZE = MPI::size(MPI_COMM_WORLD);
     auto MPI_RANK = MPI::rank(MPI_COMM_WORLD);
 
-    if(isSolid){
-        assert(function->function_space()==displacement->function_space());
+    if (isSolid)
+    {
         std::vector<double> points;
         std::vector<double> weights;
         std::vector<double> values_function;
         std::vector<double> values_displace;
         evaluation_at_gauss_points(function, points, values_function, weights);
-        weights.clear(); points.clear();
+        weights.clear();
+        points.clear();
         evaluation_at_gauss_points(displacement, points, values_displace, weights);
         /// write the results back to the parameters.
-        if (MPI_RANK == 0) {
-            for(size_t i = 0; i < weights.size(); i++){
-                coordinates.push_back(static_cast<float>(values_displace[3*i+0]));
-                coordinates.push_back(static_cast<float>(values_displace[3*i+1]));
-                coordinates.push_back(static_cast<float>(values_displace[3*i+2]));
-                values_weights.push_back(static_cast<float>(values_function[3*i+0]));
-                values_weights.push_back(static_cast<float>(values_function[3*i+1]));
-                values_weights.push_back(static_cast<float>(values_function[3*i+2]));
+        if (MPI_RANK == 0)
+        {
+            for (size_t i = 0; i < weights.size(); i++)
+            {
+                coordinates.push_back(static_cast<float>(values_displace[3 * i + 0]));
+                coordinates.push_back(static_cast<float>(values_displace[3 * i + 1]));
+                coordinates.push_back(static_cast<float>(values_displace[3 * i + 2]));
+                values_weights.push_back(static_cast<float>(values_function[3 * i + 0]));
+                values_weights.push_back(static_cast<float>(values_function[3 * i + 1]));
+                values_weights.push_back(static_cast<float>(values_function[3 * i + 2]));
                 values_weights.push_back(static_cast<float>(weights[i]));
             }
-        } else {
-            assert(coordinates.size() == 0);
-            assert(values_weights.size() == 0);
-            assert(points.size() == 0);
-            assert(weights.size() == 0);
-            assert(values_displace.size() == 0);
-            assert(values_function.size() == 0);
         }
-    } 
-    
-    
-    else {
+    }
+
+    else
+    {
         std::vector<double> points;
         std::vector<double> weights;
         std::vector<double> values;
         evaluation_at_gauss_points(function, points, values, weights);
-        if (MPI_RANK==0){
-            for(size_t i = 0; i<weights.size(); i++){
-                coordinates.push_back(static_cast<float>(points[3*i+0]));
-                coordinates.push_back(static_cast<float>(points[3*i+1]));
-                coordinates.push_back(static_cast<float>(points[3*i+2]));
-                values_weights.push_back(static_cast<float>(values[3*i+0]));
-                values_weights.push_back(static_cast<float>(values[3*i+1]));
-                values_weights.push_back(static_cast<float>(values[3*i+2]));
+        if (MPI_RANK == 0)
+        {
+            for (size_t i = 0; i < weights.size(); i++)
+            {
+                coordinates.push_back(static_cast<float>(points[3 * i + 0]));
+                coordinates.push_back(static_cast<float>(points[3 * i + 1]));
+                coordinates.push_back(static_cast<float>(points[3 * i + 2]));
+                values_weights.push_back(static_cast<float>(values[3 * i + 0]));
+                values_weights.push_back(static_cast<float>(values[3 * i + 1]));
+                values_weights.push_back(static_cast<float>(values[3 * i + 2]));
                 values_weights.push_back(static_cast<float>(weights[i]));
             }
-        } else {
-            assert(coordinates.size() == 0);
-            assert(values_weights.size() == 0);
-            assert(points.size() == 0);
-            assert(weights.size() == 0);
-            assert(values.size() == 0);
         }
     }
-    
-    assert(values_weights.size() * 3 == coordinates.size() * 4);
 }
 
 void interpolate(std::shared_ptr<const Function> f, // interpolation function
@@ -218,34 +202,30 @@ void interpolate(std::shared_ptr<const Function> f, // interpolation function
                  bool isSolid)                      // are f and d functions on solid?
 {
     radius = input_radius;
-    quadrature_order = input_order;    
+    quadrature_order = input_order;
     auto MPI_RANK = MPI::rank(MPI_COMM_WORLD);
     auto MPI_SIZE = MPI::size(MPI_COMM_WORLD);
 
-    // generate points and values on mesh.    
+    // generate points and values on mesh.
     // find the minum point and maximun point.
-    Point min, max;
     auto coord_f_local = f->function_space()->tabulate_dof_coordinates();
     auto coord_g_local = g->function_space()->tabulate_dof_coordinates();
     std::vector<double> coord_f;
     std::vector<double> coord_g;
+
     /// gather at processor zero.
-    MPI::gather(MPI_COMM_WORLD, coord_f_local, coord_f, 0);   
+    MPI::gather(MPI_COMM_WORLD, coord_f_local, coord_f, 0);
     MPI::gather(MPI_COMM_WORLD, coord_g_local, coord_g, 0);
-    if(MPI_RANK == 0) find_min_max_points(coord_f, coord_g, min, max);
 
-    if(MPI_SIZE == 1) std::cout<<min<<max<<std::endl;
-    else if (MPI_RANK == 0) std::cout<<min<<max<<std::endl;
+    Point min, max;
+    if (MPI_RANK == 0)
+        find_min_max_points(coord_f, coord_g, min, max);
 
-    // calculate positions, values, weights(if isSolid is true,
-    // f and d are on the solid and the pos_new will be evaluated on d)
+    // calculate positions, values, weights. if isSolid is true, f and d
+    // are function on the solid and the pos_new should be evaluated on d
     std::vector<float> pos_old;
     std::vector<float> val_old;
     get_gauss_rule(isSolid, f, d, pos_old, val_old);
-    if(MPI_RANK != 0){
-        assert(pos_old.size()==0);
-        assert(val_old.size()==0);
-    }
 
     // generate pos_new on mesh_new and allocate memory for val_new.
     // (if isSolid is true, f and d are solid and g is on fluid)
@@ -254,7 +234,8 @@ void interpolate(std::shared_ptr<const Function> f, // interpolation function
     if (isSolid)
     {
         /// get the dof coordinates of fluid function.
-        if (MPI_RANK == 0) {
+        if (MPI_RANK == 0)
+        {
             pos_new.resize(coord_g.size() / 3);
             for (size_t i = 0; i < coord_g.size() / 9; i++)
             {
@@ -263,9 +244,6 @@ void interpolate(std::shared_ptr<const Function> f, // interpolation function
                 pos_new[3 * i + 2] = static_cast<float>(coord_g[9 * i + 2]);
             }
             val_new.resize(pos_new.size());
-        } else {
-            assert(pos_new.size() == 0);
-            assert(val_new.size() == 0);
         }
     }
     else
@@ -275,39 +253,41 @@ void interpolate(std::shared_ptr<const Function> f, // interpolation function
         std::vector<double> temp_pos_new;
         std::vector<float> pos_new_local;
         d->vector()->get_local(temp_pos_new);
-        vectorTypeConvert(temp_pos_new,pos_new_local);
-        MPI::gather(MPI_COMM_WORLD,pos_new_local, pos_new, 0);
-        if(MPI_RANK == 0){
+        vectorTypeConvert(temp_pos_new, pos_new_local);
+        MPI::gather(MPI_COMM_WORLD, pos_new_local, pos_new, 0);
+        if (MPI_RANK == 0)
+        {
             val_new.resize(pos_new.size());
-        } else {
-            assert(val_new.size() == 0);
-            assert(pos_new.size() == 0);
         }
     }
-    
-    if (MPI_RANK == 0){
+
+    if (MPI_RANK == 0)
+    {
         // use the particle system for delta interpolation.
         ParticleSystem particle_system(pos_old.size() / 3, radius, min[0], min[1], min[2], max[0] - min[0], max[1] - min[1], max[2] - min[2]);
         particle_system.inputData(pos_old.data(), val_old.data());
         particle_system.interpolate(pos_new.size() / 3, pos_new.data(), val_new.data());
     }
 
-    /// collect the local range of every processor. 
+    /// collect the local range of every processor.
     auto temp_range = g->vector()->local_range();
-    std::vector<size_t> single_range{temp_range.first, temp_range.second};
-    std::vector<size_t> global_range;
+    std::vector<int64_t> single_range{temp_range.first, temp_range.second};
+    std::vector<int64_t> global_range;
     MPI::gather(MPI_COMM_WORLD, single_range, global_range, 0);
 
     /// prepare data structures for sending.
     std::vector<std::vector<double>> values_send;
-    if (MPI_RANK == 0) {
-        for(size_t i = 0; i < MPI_SIZE; i++){
-            auto offset = global_range[2*i];
-            auto local_size = global_range[2*i+1]-global_range[2*i];
+    if (MPI_RANK == 0)
+    {
+        for (size_t i = 0; i < MPI_SIZE; i++)
+        {
+            auto offset = global_range[2 * i];
+            auto local_size = global_range[2 * i + 1] - global_range[2 * i];
             std::vector<double> temp_values(local_size);
-            for (size_t j = 0; j < local_size; j++) {
-		        temp_values[j] = val_new[offset+j];
-	        }
+            for (size_t j = 0; j < local_size; j++)
+            {
+                temp_values[j] = val_new[offset + j];
+            }
             values_send.push_back(temp_values);
         }
     }
