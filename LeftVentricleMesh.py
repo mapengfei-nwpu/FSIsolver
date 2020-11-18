c6e86cb8ddb11554407fecaa1bf681f0a770679d
Your Name <you@example.com>
2020-11-18 14:26:25 +0800

    create the mesh and define the boundary.

---
+++ LeftVentricleMesh.py
@@ -0,0 +1,48 @@
+# triangulation
+from fenics import *
+from mshr import *
+
+mesh_size = 50
+big = Ellipsoid(Point(0,0,0),10,10,20)
+small = Ellipsoid(Point(0,0,0),7,7,17)
+gaizi = Box(Point(-11,-11,5),Point(11,11,21))
+domain = big-small-gaizi
+mesh = generate_mesh(domain, mesh_size)
+
+# mark the boundary
+mesh_function = MeshFunction("size_t", mesh, 2, value = 0)
+
+class VentricleBase(SubDomain):
+    def inside(self, x, on_boundary):
+        return on_boundary and near(x[2],5.0)
+
+
+class VentricleWall(SubDomain):
+    def inside(self, x, on_boundary):
+        temp = (x[0]*x[0] + x[1]*x[1])/7.0/7.0 + x[2]*x[2]/17.0/17.0
+        return on_boundary and temp < 1.1
+
+
+venbase = VentricleBase()
+venbase.mark(mesh_function, 1)
+venwall = VentricleWall()
+venwall.mark(mesh_function, 2)
+
+# visulize mesh_function
+V = FunctionSpace(mesh, "P", 1)
+U = VectorFunctionSpace(mesh, "P", 1)
+
+
+bcs = [DirichletBC(V, Constant(100), mesh_function, 1),
+      DirichletBC(V, Constant(10000), mesh_function, 2)]
+
+bcs_component = [DirichletBC(U.sub(0), Constant(10000), mesh_function, 2)]
+
+v = Function(V)
+u = Function(u)
+
+for bc in bcs:
+    bc.apply(v.vector())
+
+for bc in bcs_component:
+    bc.apply(u.vector())
